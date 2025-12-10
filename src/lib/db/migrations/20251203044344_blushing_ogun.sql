9b9e23ff848854793c1d34de6b0cefae186b6d0b
Marthayaputra <marthayaputra@hotmail.com>
2025-12-10 10:51:28 +0800

    fix: migration

--- src/lib/db/migrations/20251203044344_blushing_ogun.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "user_people" ADD COLUMN "profile_path" text;--> statement-breakpoint
-ALTER TABLE "user_preferences" ADD COLUMN "poster_path" text;
--- src/lib/db/migrations/20251204041444_serious_stepford_cuckoos.sql
+++
@@ -1,5 +0,0 @@
--- Clear existing preferences data since year field is now required
-DELETE FROM "user_preferences";
-
--- Add non-nullable year column
-ALTER TABLE "user_preferences" ADD COLUMN "year" integer NOT NULL;
