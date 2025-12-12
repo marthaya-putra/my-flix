652037de503c659f719720f6bd666bb75a9381e7
Marthayaputra <marthayaputra@hotmail.com>
2025-12-12 12:48:07 +0800

    feat: add wrangler

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,18 @@ import tsConfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 import viteReact from "@vitejs/plugin-react";
+import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
   server: {
     port: 3000,
   },
   plugins: [
+    cloudflare({
+      viteEnvironment: {
+        name: "ssr", // Recommended setting for TanStack Start SSR
+      },
+    }),
     tsConfigPaths(),
     tanstackStart(),
     // react's vite plugin must come after start's vite plugin
