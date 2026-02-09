100ee51ce67cde5441b44b4df7ba68d210672265
Marthayaputra <marthayaputra@hotmail.com>
2026-02-09 14:59:39 +0700

    feat: update streaming link

--- src/components/play-link.tsx
+++ src/components/play-link.tsx
@@ -7,7 +7,7 @@ interface PlayLinkProps {
 export function PlayLink({ title, category, children }: PlayLinkProps) {
   return (
     <a
-      href={`https://123movies-official.hair/search?q=${title.replace(/ /g, "+")}&category=${category}`}
+      href={`https://fmoviesz.fi/search/${title.replace(/ /g, "-")}}`}
       target={title}
       rel="noopener noreferrer"
       className="block text-decoration-none color-inherit"
