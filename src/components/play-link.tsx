e3d65f1869926e1fca7fbf9baf9b8315f7cab106
Marthayaputra <marthayaputra@hotmail.com>
2026-02-12 14:20:27 +0700

    fix: play link

--- src/components/play-link.tsx
+++ src/components/play-link.tsx
@@ -7,7 +7,7 @@ interface PlayLinkProps {
 export function PlayLink({ title, category, children }: PlayLinkProps) {
   return (
     <a
-      href={`https://fmoviesz.fi/search/${title.replace(/ /g, "-")}}`}
+      href={`https://fmoviesz.fi/search/${title.replace(/ /g, "-")}`}
       target={title}
       rel="noopener noreferrer"
       className="block text-decoration-none color-inherit"
